3409158f9ba4fd7f878f2db64c41411ee4e6bbc3
AaRrJjUuNn <92224840+AaRrJjUuNn@users.noreply.github.com>
2021-10-09 19:07:05 +0200

    Add files via upload

---
+++ xno.py
@@ -0,0 +1,12 @@
+import random
+import indexfunction
+r1 = [0, 0, 0]
+r2 = [0, 0, 0]
+r3 = [0, 0, 0]
+
+print(indexfunction.change_index(r1, r2, r3))
+
+
+print('-'*100)
+# 1 now stored between 3
+# call function till no 0 is left
